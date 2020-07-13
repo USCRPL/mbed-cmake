c3ed9af6d0b30ab05b39d432f14cc205699bd09c
Luka <luka.govedic@gmail.com>
2020-07-12 23:29:45 -0400

    Removed proxy header files.

--- mbed-src/config-headers/mbed_config.h
+++
@@ -1,2 +0,0 @@
-// Just a proxy header. MBed's CMakeLists.txt will look for this file directly, so we need to tell it where to find the generated config
-#include "../../../mbed-cmake-config/config-headers/mbed_config_generated.h"
